7c6a3627ce34fe76e3547ea6b50b37faa6f4ec1f
Pablo Diez <pablo@zauberlabs.com>
2012-04-18 19:40:03 -0300

    Adding test driver

---
+++ src/test/java/org/mule/module/facebook/FacebookConnectorTestDriver.java
@@ -0,0 +1,58 @@
+/**
+ * Mule Facebook Cloud Connector
+ *
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.module.facebook;
+
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test Driver for the connector
+ */
+public class FacebookConnectorTestDriver
+{
+    /**  */
+    private static final String ACCESS_TOKEN = "";
+    private FacebookConnector connector;
+
+    @Before
+    public void setup()
+    {
+        connector = new FacebookConnector();
+    }
+
+    @Test
+    public void testGetPhoto() throws Exception
+    {
+        final Map<String, Object> res = connector.getPhoto("4", "");
+        assertNotNull(res.get("link"));
+    }
+    
+    @SuppressWarnings("unchecked")
+    @Test
+    public void getAlbum()
+    {
+        final Map<String, Object> res = connector.getAlbum("20531316728", "");
+        assertNotNull(((Map<String, Object>)res.get("cover")).get("cover_id"));
+    }
+    
+    @Test
+    public void testPublishMessage() throws Exception
+    {
+        final Map<String, Object> res = connector.publishMessage(ACCESS_TOKEN, "me",
+            "testFacebookConnector", "", "", "", "", "");
+        assertNotNull(res);
+    }
+
+}
