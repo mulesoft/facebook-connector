8b43e213615482fc1cfc9612f793ec6737be455c
Pablo Diez <pablo@zauberlabs.com>
2012-07-02 14:15:52 -0300

    Adding test driver for search, album, events and groups operations

--- src/test/java/org/mule/module/facebook/FacebookConnectorTestDriver.java
+++ src/test/java/org/mule/module/facebook/FacebookConnectorTestDriver.java
@@ -13,11 +13,21 @@ package org.mule.module.facebook;
 import static org.junit.Assert.*;
 
 import java.util.List;
-import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.mule.module.facebook.types.Member;
+
+import com.restfb.types.Album;
+import com.restfb.types.Checkin;
+import com.restfb.types.Comment;
+import com.restfb.types.Event;
+import com.restfb.types.Group;
+import com.restfb.types.Page;
+import com.restfb.types.Photo;
+import com.restfb.types.Post;
+import com.restfb.types.User;
 
 /**
  * Test Driver for the connector
@@ -25,7 +35,7 @@ import org.junit.Test;
 public class FacebookConnectorTestDriver
 {
     /**  */
-    private static final String ACCESS_TOKEN = System.getenv("facebookAccessToken");
+    private static final String ACCESS_TOKEN = "AAAAAAITEghMBAJ0dypJ21fXjNdiuihxassIpnKSMsf1hbka0508OyD0cqyoq8eWi6DeTfhEO4MTlHkgZCcBNwUNaHGn1v0L8p9VQr9XNI3wYFZAbrd";
     private FacebookConnector connector;
 
     @Before
@@ -33,13 +43,222 @@ public class FacebookConnectorTestDriver
     {
         connector = new FacebookConnector();
     }
-
-    @SuppressWarnings("unchecked")
+    
     @Test
-    public void testGetPhoto() throws Exception
+    public void loggedUserDetails()
     {
-        final Map<String, Object> res = connector.getPhoto("347528201978388", "");
-        assertNotNull((((List<Map<String, Object>>) res.get("images")).get(0)).get("source"));
+        User user = connector.loggedUserDetails(ACCESS_TOKEN);
+        assertNotNull(user.getId());
+    }
+    
+    @Test
+    public void searchPosts()
+    {
+        List<Post> posts = connector.searchPosts("chacarita", "", "", "1", "2");
+        assertTrue(posts.size() > 0);
+        assertNotNull(posts.get(0).getId());
+    }
+    
+    @Test
+    public void searchUsers()
+    {
+        List<User> users = connector.searchUsers(ACCESS_TOKEN, "Norris", "", "", "1", "2");
+        assertTrue(users.size() > 0);
+        assertNotNull(users.get(0).getId());
+    }
+    
+    @Test
+    public void searchPages()
+    {
+        List<Page> pages = connector.searchPages("Norris", "", "", "2", "1");
+        assertTrue(pages.size() == 2);
+        assertNotNull(pages.get(0).getId());
+        assertNotNull(pages.get(1).getId());
+    }
+    
+    @Test
+    public void searchEvents()
+    {
+        List<Event> events = connector.searchEvents(ACCESS_TOKEN, "facebook", "", "", "2", "");
+        assertTrue(events.size() == 2);
+        for (Event event : events)
+        {
+            assertNotNull(event.getId());
+            assertNotNull(event.getName());
+        }
+    }
+    
+    @Test
+    public void searchGroups()
+    {
+        List<Group> groups = connector.searchGroups(ACCESS_TOKEN, "programming", "", "", "3", "");
+        assertTrue(groups.size() == 3);
+        for (Group group : groups)
+        {
+            assertNotNull(group.getId());
+            assertNotNull(group.getName());
+        }
+    }
+    
+    @Test
+    public void searchCheckins()
+    {
+        List<Checkin> checkins = connector.searchCheckins(ACCESS_TOKEN, "", "", "2", "");
+        assertNotNull(checkins);
+    }
+    
+    @Test
+    public void getAlbum()
+    {
+        final Album res = connector.getAlbum("99394368305", "");
+        assertNotNull(res.getId());
+        assertNotNull(res.getName());
+        assertNotNull(res.getCoverPhoto());
+        assertNotNull(res.getUpdatedTime());
+    }
+    
+    @Test
+    public void getAlbumPhotos()
+    {
+        List<Photo> photos = connector.getAlbumPhotos("99394368305", "", "", "3", "");
+        assertTrue(photos.size() == 3);
+        for (Photo photo : photos)
+        {
+            assertNotNull(photo.getId());
+            assertNotNull(photo.getFrom());
+            assertNotNull(photo.getPicture());
+        }
+    }
+    
+    @Test
+    public void getAlbumComments()
+    {
+        List<Comment> comments = connector.getAlbumComments("99394368305", "", "", "3", "");
+        assertTrue(comments.size() == 3);
+        for (Comment comment : comments)
+        {
+            assertNotNull(comment.getId());
+            assertNotNull(comment.getFrom());
+            assertNotNull(comment.getMessage());
+        }
+    }
+    
+    @Test
+    public void getEvent()
+    {
+        final Event event = connector.getEvent("331218348435", "");
+        assertNotNull(event);
+        assertNotNull(event.getDescription());
+        assertNotNull(event.getStartTime());
+    }
+    
+    @Test
+    public void getEventWall()
+    {
+        List<Post> posts = connector.getEventWall(ACCESS_TOKEN, "234960973192305", "", "", "3", "");
+        assertTrue(posts.size() == 3);
+        for (Post post : posts)
+        {
+            assertNotNull(post.getId());
+            assertNotNull(post.getFrom());
+            assertNotNull(post.getMessage());
+        }
+    }
+    
+    @Test
+    public void getEventNoReply()
+    {
+        List<User> users = connector.getEventNoReply(ACCESS_TOKEN, "234960973192305", "", "", "2", "");
+        assertTrue(users.size() == 2);
+        for (User user : users)
+        {
+            assertNotNull(user.getId());
+            assertNotNull(user.getName());
+        }
+    }
+    
+    @Test
+    public void getEventMaybe()
+    {
+        List<User> users = connector.getEventMaybe(ACCESS_TOKEN, "234960973192305", "", "", "2", "");
+        assertTrue(users.size() == 2);
+        for (User user : users)
+        {
+            assertNotNull(user.getId());
+            assertNotNull(user.getName());
+        }
+    }
+    
+    @Test
+    public void getEventInvited()
+    {
+        List<User> users = connector.getEventInvited(ACCESS_TOKEN, "234960973192305", "", "", "2", "");
+        assertTrue(users.size() == 2);
+        for (User user : users)
+        {
+            assertNotNull(user.getId());
+            assertNotNull(user.getName());
+        }
+    }
+    
+    @Test
+    public void getEventAttending()
+    {
+        List<User> users = connector.getEventAttending(ACCESS_TOKEN, "234960973192305", "", "", "2", "");
+        assertTrue(users.size() == 2);
+        for (User user : users)
+        {
+            assertNotNull(user.getId());
+            assertNotNull(user.getName());
+        }
+    }
+    
+    @Test
+    public void getEventPicture() throws Exception
+    {
+        final Byte[] res = connector.getEventPicture("234960973192305", "small");
+        assertNotNull(res);
+    }
+    
+    @Test
+    public void getGroup() throws Exception
+    {
+        final Group res = connector.getGroup("195466193802264", "");
+        assertNotNull(res.getId());
+        assertNotNull(res.getDescription());
+        assertNotNull(res.getOwner().getName());
+    }
+    
+    @Test
+    public void getGroupWall()
+    {
+        List<Post> posts = connector.getGroupWall(ACCESS_TOKEN, "18708376680", "", "", "", "");
+        assertTrue(posts.size() > 0);
+        for (Post post : posts)
+        {
+            assertNotNull(post.getId());
+            assertNotNull(post.getFrom().getName());
+        }
+    }
+    
+    @Test
+    public void getGroupMembers()
+    {
+        List<Member> members = connector.getGroupMembers(ACCESS_TOKEN, "18708376680", "", "", "", "");
+        assertTrue(members.size() > 0);
+        for (Member memeber : members)
+        {
+            assertNotNull(memeber.getId());
+            assertNotNull(memeber.getName());
+            assertNotNull(memeber.getAdministrator());
+        }
+    }
+    
+    @Test
+    public void getGroupPicture() throws Exception
+    {
+        final Byte[] res = connector.getGroupPicture("18708376680", "small");
+        assertNotNull(res);
     }
     
     @Test
@@ -49,27 +268,14 @@ public class FacebookConnectorTestDriver
         assertNotNull(res);
     }
 
-    @SuppressWarnings("unchecked")
-    @Test
-    public void getAlbum()
-    {
-        final Map<String, Object> res = connector.getAlbum("20531316728", "");
-        assertNotNull(((Map<String, Object>) res.get("cover")).get("cover_id"));
-    }
 
     @Test
     @Ignore
     public void testPublishMessage() throws Exception
     {
-        final Map<String, Object> res = connector.publishMessage(ACCESS_TOKEN, "me", "testFacebookConnector6",
+        final String res = connector.publishMessage(ACCESS_TOKEN, "me", "testFacebookConnector6",
             "", "", "", "", "");
         assertNotNull(res);
-    }
-    
-    @Test
-    public void testLoggedUserDetails()
-    {
-    	assertNotNull(connector.loggedUserDetails( ACCESS_TOKEN));
     }
     
     @Test
