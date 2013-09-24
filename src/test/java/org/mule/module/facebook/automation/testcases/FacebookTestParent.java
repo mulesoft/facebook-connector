d6013e1e6647808ae70521bff8725b3d826c1271
andreschembri <andre.schembri@ricston.com>
2013-09-24 14:04:19 +0200

    Added functionality to add comment.

--- src/test/java/org/mule/module/facebook/automation/testcases/FacebookTestParent.java
+++ src/test/java/org/mule/module/facebook/automation/testcases/FacebookTestParent.java
@@ -8,7 +8,10 @@
 
 package org.mule.module.facebook.automation.testcases;
 
+import static org.junit.Assert.fail;
+
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.Before;
@@ -16,6 +19,7 @@ import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.rules.Timeout;
 import org.mule.api.MuleEvent;
+import org.mule.api.MuleException;
 import org.mule.api.config.MuleProperties;
 import org.mule.api.processor.MessageProcessor;
 import org.mule.api.store.ObjectStore;
@@ -28,75 +32,93 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.restfb.types.Album;
 import com.restfb.types.User;
 
-
-
 public class FacebookTestParent extends TestParent {
-	
-	protected static final String[] SPRING_CONFIG_FILES = new String[] {"AutomationSpringBeans.xml"};
+
+	protected static final String[] SPRING_CONFIG_FILES = new String[] { "AutomationSpringBeans.xml" };
 	protected static ApplicationContext context;
-	protected Map<String,Object> testObjects;
+	protected Map<String, Object> testObjects;
 
 	// Set global timeout of tests to 10minutes
-    @Rule
-    public Timeout globalTimeout = new Timeout(600000);
-	
+	@Rule
+	public Timeout globalTimeout = new Timeout(600000);
+
 	@Override
 	protected String getConfigResources() {
 		return "automation-test-flows.xml";
 	}
-	
-    @BeforeClass
-    public static void beforeClass() {
-    	
-    	context = new ClassPathXmlApplicationContext(SPRING_CONFIG_FILES);
-    }
-    
-    @Before
-    public void init() throws ObjectStoreException {
-    	
-    	ObjectStore objectStore = muleContext.getRegistry().lookupObject(MuleProperties.DEFAULT_USER_OBJECT_STORE_NAME);
-    	objectStore.store("accessTokenId", (FacebookConnectorOAuthState) context.getBean("connectorOAuthState"));
 
-    }
-    
-    protected Album requestAlbum(String albumId) throws Exception {
-    	testObjects.put("album", albumId);
-    	
+	@BeforeClass
+	public static void beforeClass() {
+
+		context = new ClassPathXmlApplicationContext(SPRING_CONFIG_FILES);
+	}
+
+	@Before
+	public void init() throws ObjectStoreException {
+
+		ObjectStore objectStore = muleContext.getRegistry().lookupObject(
+				MuleProperties.DEFAULT_USER_OBJECT_STORE_NAME);
+		objectStore.store("accessTokenId",
+				(FacebookConnectorOAuthState) context
+						.getBean("connectorOAuthState"));
+
+	}
+
+	protected Album requestAlbum(String albumId) throws Exception {
+		testObjects.put("album", albumId);
+
 		MessageProcessor flow = lookupFlowConstruct("get-album");
 		MuleEvent response = flow.process(getTestEvent(testObjects));
 		return (Album) response.getMessage().getPayload();
-    }
-    
-    protected void publishAlbum(String albumName, String msg, String profileId) throws Exception {
-    	testObjects.put("albumName", albumName);
-    	testObjects.put("msg", msg);
-    	testObjects.put("profileId", profileId);
-    	
+	}
+
+	protected void publishAlbum(String albumName, String msg, String profileId)
+			throws Exception {
+		testObjects.put("albumName", albumName);
+		testObjects.put("msg", msg);
+		testObjects.put("profileId", profileId);
+
 		MessageProcessor flow = lookupFlowConstruct("publish-album");
 		flow.process(getTestEvent(testObjects));
-    }
-    
-    @SuppressWarnings("unchecked")
-	protected Collection<Album> requestUserAlbums(String user, String since, String until, String limit, String offset) throws Exception {
-    	testObjects.put("user", user);
-    	testObjects.put("since", since);
-    	testObjects.put("until", until);
-    	testObjects.put("limit", limit);
-    	testObjects.put("offset", offset);
-    	
+	}
+
+	@SuppressWarnings("unchecked")
+	public String publishMessage(String ProfileID, String Msg)
+			throws MuleException, Exception {
+
+		testObjects.put("profileId", ProfileID);
+		testObjects.put("msg", Msg);
+
+		MessageProcessor flow = lookupFlowConstruct("publish-message");
+
+		MuleEvent response = flow.process(getTestEvent(testObjects));
+		String objectID = (String) response.getMessage().getPayload();
+
+		return objectID;
+	}
+
+	@SuppressWarnings("unchecked")
+	protected Collection<Album> requestUserAlbums(String user, String since,
+			String until, String limit, String offset) throws Exception {
+		testObjects.put("user", user);
+		testObjects.put("since", since);
+		testObjects.put("until", until);
+		testObjects.put("limit", limit);
+		testObjects.put("offset", offset);
+
 		MessageProcessor flow = lookupFlowConstruct("get-user-albums");
 		MuleEvent response = flow.process(getTestEvent(testObjects));
 		return (Collection<Album>) response.getMessage().getPayload();
-    }
-    
-    public User getLoggedUserDetails() throws Exception {
-    	MessageProcessor flow = lookupFlowConstruct("logged-user-details");
-    	MuleEvent response = flow.process(getTestEvent(testObjects));
-    	return (User) response.getMessage().getPayload();
-    }
-    
-    public String getProfileId() throws Exception {
-    	return getLoggedUserDetails().getId();
-    }
-    
+	}
+
+	public User getLoggedUserDetails() throws Exception {
+		MessageProcessor flow = lookupFlowConstruct("logged-user-details");
+		MuleEvent response = flow.process(getTestEvent(testObjects));
+		return (User) response.getMessage().getPayload();
+	}
+
+	public String getProfileId() throws Exception {
+		return getLoggedUserDetails().getId();
+	}
+
 }
