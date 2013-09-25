2b244583a0db4cf84f4902ece9b60a977a4d5329
jsaliba <justin.saliba@ricston.com>
2013-09-25 13:29:59 +0200

    Updated publish comment test case

--- src/test/java/org/mule/module/facebook/automation/testcases/PublishComment.java
+++ src/test/java/org/mule/module/facebook/automation/testcases/PublishComment.java
@@ -1,13 +1,14 @@
 package org.mule.module.facebook.automation.testcases;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.util.HashMap;
 import java.util.List;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.mule.api.MuleEvent;
@@ -16,60 +17,48 @@ import org.mule.api.processor.MessageProcessor;
 import com.restfb.types.Comment;
 
 public class PublishComment extends FacebookTestParent {
+	
 	@SuppressWarnings("unchecked")
-	@BeforeClass
+	@Before
 	public void setUp() {
-		
-		//create comment for message
-		testObjects = (HashMap<String, Object>) context
-				.getBean("publishCommentTestData");
-
 		try {
-			testObjects.put("postId", publishMessage(testObjects.get("profileId")
-					.toString(), testObjects.get("statusmsg").toString()));
+			//create comment for message
+			testObjects = (HashMap<String, Object>) context.getBean("publishCommentTestData");
+			
+			String statusMsg = (String) testObjects.get("msg");
+			String postId = publishMessage(getProfileId(), statusMsg);
+			testObjects.put("postId", postId);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			fail();
 		}
 
 	}
 
-	@Category({ RegressionTests.class })
+	@Category({ SmokeTests.class, RegressionTests.class })
 	@Test
 	public void PublishCommentTestCase() {
-
-		MessageProcessor flow = lookupFlowConstruct("publish-comment");
-
 		try {
-
+			String postId = (String) testObjects.get("postId");
+			String msg = (String) testObjects.get("msg");
+			
+			MessageProcessor flow = lookupFlowConstruct("publish-comment");
 			MuleEvent response = flow.process(getTestEvent(testObjects));
 
-			String commentID = (String) response.getMessage().getPayload();
+			String commentJson = (String) response.getMessage().getPayload();
+			String commentId =  FacebookConnectorTestUtils.getId(commentJson);
 
-			testObjects.put("commentID",
-					FacebookConnectorTestUtils.getId(commentID));
-
-			List<Comment> comments = getStatusComments(testObjects
-					.get("postId").toString());
-
+			List<Comment> comments = getStatusComments(postId);
 			boolean commentFound = false;
-
 			for (Comment comment : comments) {
-
-				if (comment.getId().equals(
-						testObjects.get("commentID").toString())) {
+				if (comment.getId().equals(commentId)) {
 					commentFound = true;
-					assertEquals(testObjects.get("msg").toString(),
-							comment.getMessage());
+					assertEquals(msg, comment.getMessage());
+					break;
 				}
 			}
 
-			if (commentFound == false) {
-				fail("Comment Not found");
-
-			}
-
+			assertTrue("Comment not found", commentFound);
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail();
@@ -77,19 +66,11 @@ public class PublishComment extends FacebookTestParent {
 
 	}
 	
-	
-	
-	
-	@AfterClass
+	@After
 	public void tearDown(){
-		
-		
-		//Delete comment, Delete Message
 		try {
-			deleteObject(testObjects.get("commentID").toString());
 			deleteObject(testObjects.get("postId").toString());
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
