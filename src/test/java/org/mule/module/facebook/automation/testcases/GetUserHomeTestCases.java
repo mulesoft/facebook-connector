37e6669af052b662e4ef61e9cef418bff2164ddd
Justin Calleja <calleja.justin@gmail.com>
2013-09-26 16:00:25 +0200

    Changed GetUserHomeTestCases.

--- src/test/java/org/mule/module/facebook/automation/testcases/GetUserHomeTestCases.java
+++ src/test/java/org/mule/module/facebook/automation/testcases/GetUserHomeTestCases.java
@@ -11,32 +11,47 @@ package org.mule.module.facebook.automation.testcases;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.util.Collection;
 import java.util.HashMap;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.mule.api.MuleEvent;
 import org.mule.api.processor.MessageProcessor;
 
-import com.restfb.types.User;
+import com.restfb.types.Post;
 
 public class GetUserHomeTestCases extends FacebookTestParent {
+	
+	
+	@SuppressWarnings("unchecked")
+	@Before
+	public void setUp() {
+		try {
+	    	testObjects = (HashMap<String,Object>) context.getBean("getUserHomeTestData");
+			
+	    	String profileId = getProfileId();
+	    	testObjects.put("user", profileId);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
 	
     @SuppressWarnings("unchecked")
 	@Category({RegressionTests.class})
 	@Test
 	public void testGetUserHome() {
     	
-    	testObjects = (HashMap<String,Object>) context.getBean("getUserHomeTestData");
-    	
 		MessageProcessor flow = lookupFlowConstruct("get-user-home");
     	
 		try {
 
 			MuleEvent response = flow.process(getTestEvent(testObjects));
-			Object user = (Object) response.getMessage().getPayload();
-
-			System.out.println();
+			Collection<Post> posts = (Collection<Post>) response.getMessage().getPayload();
+			
+			assertTrue(posts instanceof Collection);
 
 		} catch (Exception e) {
 			e.printStackTrace();
