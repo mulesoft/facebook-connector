bed45e85ca74944d8156c1549d34ac149d3c6ce5
jsaliba <justin.saliba@ricston.com>
2013-09-27 09:35:24 +0200

    Updated GetNote test cases

--- src/test/java/org/mule/module/facebook/automation/testcases/GetNoteTestCase.java
+++ src/test/java/org/mule/module/facebook/automation/testcases/GetNoteTestCase.java
@@ -1,71 +1,68 @@
 package org.mule.module.facebook.automation.testcases;
 
-	import java.util.HashMap;
-	import java.util.List;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
-	import static org.junit.Assert.*;
-	import static org.junit.Assert.assertTrue;
-	import static org.junit.Assert.fail;
+import java.util.HashMap;
 
-	import org.junit.After;
-	import org.junit.Before;
-	import org.junit.Test;
-	import org.mule.api.MuleEvent;
-	import org.mule.api.MuleException;
-	import org.mule.api.processor.MessageProcessor;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mule.api.MuleEvent;
+import org.mule.api.processor.MessageProcessor;
 
-	import com.restfb.types.Note;
-	
-	
+import com.restfb.types.Note;
+
 public class GetNoteTestCase extends FacebookTestParent {
 
-		@Before
-		public void tearUp() {
-			testObjects = (HashMap<String, Object>) context
-					.getBean("publishNoteTestData");
+	@Before
+	public void tearUp() {
+		try {
+			testObjects = (HashMap<String, Object>) context.getBean("publishNoteTestData");
+
+			String profileId = getProfileId();
+			testObjects.put("profileId", profileId);
+			
 			String msg = testObjects.get("msg").toString();
 			String subject = testObjects.get("subject").toString();
 
-			try {
-				String profileId = getProfileId();
-				testObjects.put("profileId", profileId);
-				String noteid = publishNote(getProfileId(), msg, subject);
-				testObjects.put("note", noteid);
-			} catch (Exception e) {
-
-				e.printStackTrace();
-				fail();
-			}
-		}
-
-		@Test
-		public void testGetNote() {
-			String noteId = testObjects.get("note").toString();
-
-			MessageProcessor flow = lookupFlowConstruct("get-note");
-			try {
-				
-				MuleEvent response = flow.process(getTestEvent(testObjects));
-				Note note = (Note) response.getMessage().getPayload();
-
-				assertTrue(note.getMessage().contains(testObjects.get("msg").toString()));
-				assertEquals(note.getSubject(), testObjects.get("subject").toString());	
-				
-			} catch (Exception e) {
-				fail();
-				e.printStackTrace();
-			}
-		}
-
-		@After
-		public void tearDown() {
-			String noteId = testObjects.get("note").toString();
-			try {
-				deleteObject(noteId);
-			} catch (Exception e) {
-				e.printStackTrace();
-				fail();
-			}
+			String noteid = publishNote(getProfileId(), msg, subject);
+			testObjects.put("note", noteid);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
 		}
 	}
 
+	@Test
+	public void testGetNote() {
+		try {
+			String noteId = (String) testObjects.get("note");
+			String msg = (String) testObjects.get("msg");
+			String subject = (String) testObjects.get("subject");
+			
+			MessageProcessor flow = lookupFlowConstruct("get-note");
+			MuleEvent response = flow.process(getTestEvent(testObjects));
+			Note note = (Note) response.getMessage().getPayload();
+			
+			assertEquals(noteId, note.getId());
+			assertTrue(note.getMessage().contains(msg));
+			assertEquals(note.getSubject(), subject);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+	@After
+	public void tearDown() {
+		try {
+			String noteId = (String) testObjects.get("note");
+			deleteObject(noteId);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+}
