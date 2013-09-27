6f73231d1880c04afba3282a67b84f3f3a4e72f5
jsaliba <justin.saliba@ricston.com>
2013-09-27 09:31:50 +0200

    Updated GetNoteComments test cases

--- src/test/java/org/mule/module/facebook/automation/testcases/GetNoteCommentsTestCases.java
+++ src/test/java/org/mule/module/facebook/automation/testcases/GetNoteCommentsTestCases.java
@@ -2,20 +2,18 @@ package org.mule.module.facebook.automation.testcases;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
 import java.util.HashMap;
 import java.util.List;
-
-import javax.validation.constraints.AssertTrue;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.mule.api.MuleEvent;
-import org.mule.api.MuleException;
 import org.mule.api.processor.MessageProcessor;
 
 import com.restfb.types.Comment;
-import com.restfb.types.StatusMessage;
 
 public class GetNoteCommentsTestCases extends FacebookTestParent {
 	// create note.
@@ -23,15 +21,11 @@ public class GetNoteCommentsTestCases extends FacebookTestParent {
 
 	@Before
 	public void tearUp() {
-
 		try {
-			testObjects = (HashMap<String, Object>) context
-					.getBean("getNoteCommentsTestData");
+			testObjects = (HashMap<String, Object>) context.getBean("getNoteCommentsTestData");
 			String msg = testObjects.get("msg").toString();
 			String subject = testObjects.get("subject").toString();
-			String profileId;
-			
-			profileId = getProfileId();
+			String profileId = getProfileId();
 			testObjects.put("profileId", profileId);
 			
 			String noteid = publishNote(profileId, msg, subject);
@@ -40,24 +34,19 @@ public class GetNoteCommentsTestCases extends FacebookTestParent {
 			String commentId = publishComment(noteid, msg);
 			testObjects.put("commentId", commentId);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
 
+	@Category({RegressionTests.class})
 	@Test
 	public void testGetNoteComments() {
-
-		
 		try {
-			
 			String commentId = testObjects.get("commentId").toString();
 			String msg = testObjects.get("msg").toString();
 
 			MessageProcessor flow = lookupFlowConstruct("get-note-comments");
-			MuleEvent response;
-			response = flow.process(getTestEvent(testObjects));
+			MuleEvent response = flow.process(getTestEvent(testObjects));
 			List<Comment> comments = (List<Comment>) response.getMessage().getPayload();
 
 			Boolean found = false;
@@ -66,23 +55,21 @@ public class GetNoteCommentsTestCases extends FacebookTestParent {
 				if (comment.getId().toString().equals(commentId)) {
 					assertEquals(msg, comment.getMessage());
 					found = true;
+					break;
 				}
-
 			}
 			assertTrue(found);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
-		
 	}
 	
-	
-//note cannot be deleted with delete object
+	//note cannot be deleted with delete object
 	@After
 	public void tearDown(){
 		try {
-			deleteObject(testObjects.get("note").toString());
+			String note = (String) testObjects.get("note");
+			deleteObject(note);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
