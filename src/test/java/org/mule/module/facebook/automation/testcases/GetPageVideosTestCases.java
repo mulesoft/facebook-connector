469388586d01f4fbea252f8d5eb72f4f036c4de1
Justin Calleja <calleja.justin@gmail.com>
2013-10-02 18:16:25 +0200

    Added GetPageVideosTestCase and GetPageWallTestCases.

---
+++ src/test/java/org/mule/module/facebook/automation/testcases/GetPageVideosTestCases.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) MuleSoft, Inc. All rights reserved. http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.md file.
+ */
+
+package org.mule.module.facebook.automation.testcases;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mule.api.MuleEvent;
+import org.mule.api.processor.MessageProcessor;
+
+import com.restfb.types.Video;
+
+public class GetPageVideosTestCases extends FacebookTestParent {
+	
+	@SuppressWarnings("unchecked")
+	@Before
+	public void setUp() {
+		try {
+			testObjects = (HashMap<String,Object>) context.getBean("getPageVideosTestData");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+    @SuppressWarnings("unchecked")
+	@Category({RegressionTests.class})
+	@Test
+	public void testGetPageVideos() {
+    	
+		MessageProcessor flow = lookupFlowConstruct("get-page-videos");
+    	
+		try {
+
+			MuleEvent response = flow.process(getTestEvent(testObjects));
+			List<Video> result = (List<Video>) response.getMessage().getPayload();
+			
+			assertNotNull(result);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+     
+	}
+    
+    
+}
