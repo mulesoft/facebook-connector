66c02b47df11230d74c3707538874d7d82de98f6
jsaliba <justin.saliba@ricston.com>
2013-10-09 11:35:38 +0200

    Updated DownloadImageTestCases

--- src/test/java/org/mule/module/facebook/automation/testcases/DownloadImageTestCases.java
+++ src/test/java/org/mule/module/facebook/automation/testcases/DownloadImageTestCases.java
@@ -8,38 +8,80 @@
 
 package org.mule.module.facebook.automation.testcases;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.util.HashMap;
 
+import javax.imageio.ImageIO;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.mule.api.MuleEvent;
 import org.mule.api.processor.MessageProcessor;
+import org.mule.module.facebook.types.Photo;
 
 public class DownloadImageTestCases extends FacebookTestParent {
+	
+	@Before
+	public void setUp() {
+		try {
+			testObjects = (HashMap<String,Object>) context.getBean("downloadImageTestData");
+
+			String profileId = getProfileId();
+			
+			String caption = (String) testObjects.get("caption");
+			File photoFile = new File(getClass().getClassLoader().getResource((String) testObjects.get("photoFilePath")).toURI());
+
+			String photoId = publishPhoto(profileId, caption, photoFile);
+			testObjects.put("photoId", photoId);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
 	
     @SuppressWarnings("unchecked")
 	@Category({RegressionTests.class})
 	@Test
 	public void testDownloadImage() {
+    	try {
+    		String photoId = (String) testObjects.get("photoId");
+    		Photo photo = getPhoto(photoId, "0");
+    		
+    		testObjects.put("imageUri", photo.getPicture());
+    		MessageProcessor flow = lookupFlowConstruct("download-image");
     	
-    	testObjects = (HashMap<String,Object>) context.getBean("downloadImageTestData");
-    	
-		MessageProcessor flow = lookupFlowConstruct("download-image");
-    	
-		try {
 			MuleEvent response = flow.process(getTestEvent(testObjects));
 			
 			byte[] result = (byte[]) response.getMessage().getPayload();
-			
 			assertTrue(result.length > 0);
+			
+			BufferedImage image = ImageIO.read(new ByteArrayInputStream(result));
+			assertTrue(image.getHeight() == photo.getHeight());
+			assertTrue(image.getWidth() == photo.getWidth());
+    	} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+    
+    @After
+    public void tearDown() {
+    	try {
+    		String photoId = (String) testObjects.get("photoId");
+    		deleteObject(photoId);
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail();
 		}
-     
-	}
+    }
     
 }
