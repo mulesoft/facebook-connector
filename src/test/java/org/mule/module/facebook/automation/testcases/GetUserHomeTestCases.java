bbd65d4aa2dccae497e2ac096d19d6b09ffbee2c
jsaliba <justin.saliba@ricston.com>
2013-10-14 11:56:57 +0200

    Updated GetUserHome test cases

--- src/test/java/org/mule/module/facebook/automation/testcases/GetUserHomeTestCases.java
+++ src/test/java/org/mule/module/facebook/automation/testcases/GetUserHomeTestCases.java
@@ -11,9 +11,12 @@ package org.mule.module.facebook.automation.testcases;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -26,16 +29,21 @@ public class GetUserHomeTestCases extends FacebookTestParent {
 	
 	@SuppressWarnings("unchecked")
 	@Before
-	public void setUp() {
-		try {
-	    	testObjects = (HashMap<String,Object>) context.getBean("getUserHomeTestData");
-			
-	    	String profileId = getProfileId();
-	    	testObjects.put("user", profileId);
-		} catch (Exception e) {
-			e.printStackTrace();
-			fail();
+	public void setUp() throws Exception {
+    	testObjects = (HashMap<String,Object>) context.getBean("getUserHomeTestData");
+		
+    	String profileId = getProfileId();
+    	testObjects.put("user", profileId);
+    	
+    	List<String> messages = (List<String>) testObjects.get("messages");
+    	List<String> messageIds = new ArrayList<String>();
+    	
+    	for (String message : messages) {
+			String messageId = publishMessage(profileId, message);
+			messageIds.add(messageId);
 		}
+    	
+    	testObjects.put("messageIds", messageIds);
 	}
 	
     @SuppressWarnings("unchecked")
@@ -43,15 +51,36 @@ public class GetUserHomeTestCases extends FacebookTestParent {
 	@Test
 	public void testGetUserHome() {
 		try {
+			List<String> messageIds = (List<String>) testObjects.get("messageIds");
+			
 			MessageProcessor flow = lookupFlowConstruct("get-user-home");
 			MuleEvent response = flow.process(getTestEvent(testObjects));
 
 			Collection<Post> posts = (Collection<Post>) response.getMessage().getPayload();
-			assertTrue(posts.size() > 0);
+			assertTrue(posts.size() >= messageIds.size());
+			
+			for (String messageId : messageIds) {
+				boolean found = false;
+				for (Post post : posts) {
+					if (post.getId().equals(messageId)) {
+						found = true;
+						break;
+					}
+				}
+				assertTrue(found);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail();
 		}
 	}
+    
+    @After
+    public void tearDown() throws Exception {
+    	List<String> messageIds = (List<String>) testObjects.get("messageIds");
+    	for (String messageId : messageIds) {
+			deleteObject(messageId);
+		}
+    }
     
 }
