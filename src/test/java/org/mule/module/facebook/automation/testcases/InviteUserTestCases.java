2be21732628118b50c0652f109e97198112836c2
jsaliba <justin.saliba@ricston.com>
2013-10-30 13:49:04 +0100

    Reverted InviteUser test cases to previous commit

--- src/test/java/org/mule/module/facebook/automation/testcases/InviteUserTestCases.java
+++ src/test/java/org/mule/module/facebook/automation/testcases/InviteUserTestCases.java
@@ -1,12 +1,17 @@
 package org.mule.module.facebook.automation.testcases;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+
+import com.restfb.types.User;
 
 public class InviteUserTestCases extends FacebookTestParent {
 
@@ -33,9 +38,25 @@ public class InviteUserTestCases extends FacebookTestParent {
 		try {
 			String auxProfileId = getTestRunMessageValue("auxProfileId");
 			String eventId = getTestRunMessageValue("eventId");
+			String until = getTestRunMessageValue("until");
+			String since = getTestRunMessageValue("since");
+			String offset = getTestRunMessageValue("offset");
+			String limit = getTestRunMessageValue("limit");
 
 			Boolean result = inviteUser(eventId, auxProfileId);
 			assertTrue(result);
+			
+			List<User> invited = getEventInvited(eventId, until, since, limit, offset);
+			
+			boolean found = false;
+			for (User user : invited) {
+				if (user.getId().equals(auxProfileId)) {
+					found = true;
+					break;
+				}
+			}
+			
+			assertTrue(found);
 		}
 		catch (Exception e) {
 			e.printStackTrace();
