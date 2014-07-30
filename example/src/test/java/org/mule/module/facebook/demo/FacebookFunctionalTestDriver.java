a041e2a009a44cd8b5440d2c021a70807c042e86
Karthik Anantha <karthik@whiteskylabs.com>
2014-07-30 17:49:58 +0530

    Updated license headers

--- example/src/test/java/org/mule/module/facebook/demo/FacebookFunctionalTestDriver.java
+++ example/src/test/java/org/mule/module/facebook/demo/FacebookFunctionalTestDriver.java
@@ -1,9 +1,7 @@
 /**
- * Copyright (c) MuleSoft, Inc. All rights reserved. http://www.mulesoft.com
- *
- * The software in this package is published under the terms of the CPAL v1.0
- * license, a copy of which has been included with this distribution in the
- * LICENSE.md file.
+ * (c) 2003-2014 MuleSoft, Inc. The software in this package is published under
+ * the terms of the CPAL v1.0 license, a copy of which has been included with this
+ * distribution in the LICENSE.md file.
  */
 
 package org.mule.module.facebook.demo;
